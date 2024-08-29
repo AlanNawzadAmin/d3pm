fix masking

d3pm_sc/.ipynb_checkpoints/continuous_time_diffusion-checkpoint.py
@@ -24,12 +24,15 @@ class ContinuousTimeDiffusion(nn.Module): #schedule conditioning is True!
         super().__init__()
         self.x0_model = x0_model
         self.hybrid_loss_coeff = hybrid_loss_coeff
-        self.eps = 1e-6
+        self.eps = 1e-10
         self.num_classes = num_classes
         self.t_max = 0.999
 
         # Precalculate betas
         self.alpha, self.beta = get_betas(schedule_type)
+
+    def get_stationary(self):
+        raise NotImplementedError
 
     def model_predict(self, x_0, t, cond, S=None):
         return self.x0_model(x_0, t, cond, S)
@@ -42,7 +45,7 @@ class ContinuousTimeDiffusion(nn.Module): #schedule conditioning is True!
 
     def sample_point(self, x):
         t = torch.rand(x.shape[0], device=x.device) * self.t_max
-        S = sample_n_transitions_cont(self.alpha_scale, x[0].flatten().shape[0], t)
+        S = sample_n_transitions_cont(self.alpha, x[0].flatten().shape[0], t)
         S = S.swapaxes(0, 1).reshape(*x.shape).long()
         x_t = self.x_t_sample(
             x, t, torch.rand((*x.shape, self.num_classes), device=x.device), S

d3pm_sc/.ipynb_checkpoints/ct_sched_cond-checkpoint.py
@@ -12,24 +12,49 @@ class SchdeuleCondition(ContinuousTimeDiffusion): #schedule conditioning is True
         self,
         x0_model: nn.Module,
         num_classes: int = 10,
-        forward_type="uniform",
+        forward_kwargs={"type":"uniform"},
         schedule_type="cos",
         gamma=0,
-        hybrid_loss_coeff=0.001,
+        hybrid_loss_coeff=0.01,
     ):
         # Precalculate betas, define model_predict, p_sample
         super().__init__(x0_model, num_classes, schedule_type, hybrid_loss_coeff)
         assert gamma >= 0 and gamma < 1 # full schedule and classical resp.
 
         # Precalculate Ks
-        L = get_inf_gens(forward_type, num_classes)
+        L = get_inf_gens(forward_kwargs, num_classes)
         rate = - (L.diagonal().min()) / (1-gamma) # L^* in sec 6.6 of the notes
         K = L / rate + torch.eye(num_classes)
-        self.beta_scale = lambda t: rate * self.beta(t)
-        self.alpha_scale = lambda t: self.alpha(t) ** rate
+        self.beta_scale = self.beta
+        self.beta = lambda t: rate * self.beta_scale(t)
+        self.alpha_scale = self.alpha
+        self.alpha  = lambda t: self.alpha_scale(t) ** rate
         K_powers = torch.stack([torch.linalg.matrix_power(K, i) for i in range(1000)])
         self.register_buffer("K", K)
         self.register_buffer("K_powers", K_powers)
+
+    def get_stationary(self):
+        evals, evecs = torch.linalg.eig(self.K.T)
+        assert torch.isclose(evals[torch.argmax(torch.norm(evals))], torch.tensor(1, dtype=torch.complex64))
+        stationary = evecs[:, torch.argmax(torch.norm(evals))]
+        assert torch.allclose(torch.imag(stationary), torch.tensor(0, dtype=self.K.dtype))
+        stationary = torch.real(stationary)
+        stationary = stationary * torch.sign(stationary)
+        assert torch.all(stationary > 0)
+        return stationary / stationary.sum()
+
+    def get_kl_t1(self, x):
+        # sample S
+        t = self.t_max * torch.ones(x.shape[0], device=x.device)
+        S = sample_n_transitions_cont(self.alpha, x[0].flatten().shape[0], t)
+        S = S.swapaxes(0, 1).reshape(*x.shape).long()
+        # get p(x_1|x_0, S)
+        x_0_logits = convert_to_distribution(x, self.num_classes, self.eps)
+        trans_mats = self.K_powers[S, :, :]
+        softmaxed = torch.softmax(x_0_logits, dim=-1)  # bs, ..., num_classes
+        x_1 = torch.log(torch.einsum("b...c,b...cd->b...d", softmaxed, trans_mats))
+        kl = kls(x_1, self.get_stationary(), self.eps)
+        return kl.mean()
 
     def x_t_sample(self, x_0, t, noise, S):
         # forward process, x_0 is the clean input.
@@ -61,7 +86,7 @@ class SchdeuleCondition(ContinuousTimeDiffusion): #schedule conditioning is True
 
         # get kls and loss
         kl = kls(true_q_posterior_logits, pred_q_posterior_logits, self.eps) # shape x
-        weight = - self.beta_scale(t) / torch.log(self.alpha_scale(t))
+        weight = - self.beta(t) / torch.log(self.alpha(t))
         weight = (S.swapaxes(0, -1) * weight).swapaxes(0, -1)
         vb_loss = (kl * weight).mean() * self.t_max
 
@@ -77,7 +102,7 @@ class SchdeuleCondition(ContinuousTimeDiffusion): #schedule conditioning is True
 
     def sample_with_image_sequence(self, x, cond=None, trans_step=1, stride=10):
         t = self.t_max * torch.ones(x.shape[0], device=x.device)
-        S = sample_n_transitions_cont(self.alpha_scale, x[0].flatten().shape[0], t)
+        S = sample_n_transitions_cont(self.alpha, x[0].flatten().shape[0], t)
         S = S.swapaxes(0, 1).reshape(*x.shape).long()
         steps = 0
         images = []
@@ -99,7 +124,7 @@ class SchdeuleCondition(ContinuousTimeDiffusion): #schedule conditioning is True
             pbar.update(trans_step)
             steps += 1
             if steps % stride == 0:
-                images.append(x)
+                images.append(torch.clone(x))
         pbar.close()
         # if last step is not divisible by stride, we add the last image.
         if steps % stride != 0:

d3pm_sc/.ipynb_checkpoints/d3pm_classic-checkpoint.py
@@ -12,7 +12,7 @@ class D3PM_classic(DiscreteTimeDiffusion):
         x0_model: nn.Module,
         n_T: int,
         num_classes: int = 10,
-        forward_type="uniform",
+        forward_kwargs={"type":"uniform"},
         schedule_type="cos",
         hybrid_loss_coeff=0.001,
     ):
@@ -23,11 +23,11 @@ class D3PM_classic(DiscreteTimeDiffusion):
         q_onestep_mats = []
         q_mats = []  # these are cumulative
         for beta in self.beta_t:
-            if forward_type == "uniform":
+            if forward_kwargs['type'] == "uniform":
                 mat = torch.ones(num_classes, num_classes) * beta / num_classes
                 mat.diagonal().fill_(1 - (num_classes - 1) * beta / num_classes)
                 q_onestep_mats.append(mat)
-            elif forward_type == "masking":
+            elif forward_kwargs['type'] == "masking":
                 mat = torch.eye(num_classes) * (1 - beta)
                 mat[:, -1] += beta
                 q_onestep_mats.append(mat)
@@ -43,6 +43,25 @@ class D3PM_classic(DiscreteTimeDiffusion):
         q_mats = torch.stack(q_mats, dim=0)
         self.register_buffer("q_one_step_transposed", q_one_step_transposed)
         self.register_buffer("q_mats", q_mats)
+        
+    def get_stationary(self):
+        evals, evecs = torch.linalg.eig(q_mats[-1].T)
+        assert torch.isclose(evals[torch.argmax(torch.norm(evals))], torch.tensor(1, dtype=torch.complex64))
+        stationary = evecs[:, torch.argmax(torch.norm(evals))]
+        assert torch.allclose(torch.imag(stationary), torch.tensor(0, dtype=self.K.dtype))
+        stationary = torch.real(stationary)
+        stationary = stationary * torch.sign(stationary)
+        assert torch.all(stationary > 0)
+        return stationary / stationary.sum()
+
+    def get_kl_t1(self, x):
+        # get p(x_1|x_0)
+        x_0_logits = convert_to_distribution(x, self.num_classes, self.eps)
+        trans_mats = self.q_mats[-1, :, :]
+        softmaxed = torch.softmax(x_0_logits, dim=-1)  # bs, ..., num_classes
+        x_1 = torch.einsum("b...c,...cd->b...d", softmaxed, trans_mats)
+        kl = kls(x_1, self.get_stationary(), self.eps)
+        return kl.mean()
     
     def x_t_sample(self, x_0, t, noise, S=None):
         # forward process, x_0 is the clean input.

d3pm_sc/.ipynb_checkpoints/utils-checkpoint.py
@@ -25,16 +25,19 @@ def convert_to_distribution(x_0, num_classes, eps):
         x_0_logits = x_0.clone()
     return x_0_logits
 
-def get_inf_gens(forward_type, num_classes):
-    if forward_type == "uniform":
+def get_inf_gens(forward_kwargs, num_classes):
+    if forward_kwargs['type'] == "uniform":
         L = torch.ones(num_classes, num_classes) / (num_classes-1)
         L.diagonal().fill_(-1)
-    elif forward_type == "gaussian":
-        print(1)
+    elif forward_kwargs['type'] == "gaussian":
+        bandwidth = forward_kwargs['bandwidth']
         range_ = torch.arange(num_classes)
         diff_mat = (range_[:, None] - range_[None, :]) ** 2
-        L = torch.exp(- 4 * diff_mat / (num_classes ** 2))
+        L = torch.exp(- diff_mat / (2 * (bandwidth * num_classes) ** 2))
         L = L / (L.sum(-1).max() - 1)
         L.diagonal().fill_(0)
         L[range_, range_] = -L.sum(-1)
+    if "normalize" in forward_kwargs.keys():
+        if forward_kwargs['normalize']:
+            L = L / (- L.diagonal()[:, None])
     return L

d3pm_sc/d3pm.py
@@ -7,15 +7,13 @@ from .utils import kls, convert_to_distribution
 from .schedule_sample import sample_n_transitions, sample_full_transitions
 from .discrete_time_diffusion import DiscreteTimeDiffusion
 
-# TODO: add KL(p(x_1)||q(x_1)), stop conditioning on t, different alpha schedules
-
 class D3PM(DiscreteTimeDiffusion): #schedule conditioning is True!
     def __init__(
         self,
         x0_model: nn.Module,
         n_T: int,
         num_classes: int = 10,
-        forward_type="uniform",
+        forward_kwargs={"type":"uniform"},
         schedule_type="cos",
         gamma=0,
         hybrid_loss_coeff=0.001,
@@ -25,13 +23,36 @@ class D3PM(DiscreteTimeDiffusion): #schedule conditioning is True!
         assert gamma >= 0 and gamma < 1 # full schedule and classical resp.
 
         # Precalculate Ks
-        L = get_inf_gens(forward_type, num_classes)
+        L = get_inf_gens(forward_kwargs, num_classes)
         rate = - (L.diagonal().min()) / (1-gamma) # L^* in sec 6.6 of the notes
         K = L / rate + torch.eye(num_classes)
         self.beta_t = torch.minimum(rate * self.beta_t, torch.ones_like(self.beta_t) * 0.999)
         K_powers = torch.stack([torch.linalg.matrix_power(K, i) for i in range(n_T)])
         self.register_buffer("K", K)
         self.register_buffer("K_powers", K_powers)
+
+    def get_stationary(self):
+        evals, evecs = torch.linalg.eig(self.K.T)
+        assert torch.isclose(evals[torch.argmax(torch.norm(evals))], torch.tensor(1, dtype=torch.complex64))
+        stationary = evecs[:, torch.argmax(torch.norm(evals))]
+        assert torch.allclose(torch.imag(stationary), torch.tensor(0, dtype=self.K.dtype))
+        stationary = torch.real(stationary)
+        stationary = stationary * torch.sign(stationary)
+        assert torch.all(stationary > 0)
+        return stationary / stationary.sum()
+
+    def get_kl_t1(self, x):
+        # sample S
+        t = (self.n_T-1) * torch.ones(x.shape[0], device=x.device).int()
+        S = sample_n_transitions(self.beta_t.to(x.device), x[0].flatten().shape[0], t)
+        S = S.swapaxes(0, 1).reshape(*x.shape).long()
+        # get p(x_1|x_0, S)
+        x_0_logits =convert_to_distribution(x, self.num_classes, self.eps)
+        trans_mats = self.K_powers[S, :, :]
+        softmaxed = torch.softmax(x_0_logits, dim=-1)  # bs, ..., num_classes
+        x_1 = torch.einsum("b...c,b...cd->b...d", softmaxed, trans_mats)
+        kl = kls(x_1, self.get_stationary(), self.eps)
+        return kl.mean()
 
     def x_t_sample(self, x_0, t, noise, S):
         # forward process, x_0 is the clean input.

d3pm_sc/discrete_time_diffusion.py
@@ -33,12 +33,17 @@ class DiscreteTimeDiffusion(nn.Module): #schedule conditioning is True!
         # Precalculate betas
         self.beta_t = get_betas(schedule_type, n_T)
 
+    def get_stationary(self):
+        raise NotImplementedError
+
+    def get_kl_t1(self, x):
+        raise NotImplementedError
+
     def model_predict(self, x_0, t, cond, S=None):
         return self.x0_model(x_0, t, cond, S)
 
     def q_posterior_logits(self, x_0, x_t, t, S=None):
         raise NotImplementedError
-
 
     def x_t_sample(self, x_0, t, noise, S=None):
         raise NotImplementedError

d3pm_sc/masking_diffusion.py
@@ -14,29 +14,35 @@ class MaskingDiffusion(SchdeuleCondition): #schedule conditioning is True!
         x0_model: nn.Module,
         num_classes: int = 10,
         schedule_type="cos",
-        hybrid_loss_coeff=0.001,
+        hybrid_loss_coeff=0.01,
     ):
-        forward_type = "uniform"
+        forward_kwargs={"type":"uniform"}
         gamma = 1/num_classes
-        super().__init__(x0_model, num_classes, schedule_type, hybrid_loss_coeff,
-                         gamma=gamma, forward_type=forward_type)
+        super().__init__(x0_model, num_classes, forward_kwargs, schedule_type, gamma, hybrid_loss_coeff)
         # with this choice, x_t_sample is uniform and 
         # q_posterior_logits returns uniform if S>1 and x_0 pred if S==1
+        # The only differences is the predictions and marginalizing over S>1 in the weight
+        # so we always assume S==1.
+        # in principle we could also speed up sampling by ignoring S>1
 
+    def model_predict(self, x_t, t, cond, S):
+        masked_pos = S > 0
+        masked_x_t = torch.where(masked_pos, self.num_classes, x_t)
+        return self.x0_model(masked_x_t, t, cond, S=None)[..., :-1]
+    
     def forward(self, x: torch.Tensor, cond: torch.Tensor = None) -> torch.Tensor:
         t, S, x_t = self.sample_point(x)
-        masked_pos = S > 0
-        x_t[masked_pos] = self.num_classes # mask these positions
+        S = (S>0).long()
         
         # predict x_0 and prev(x_t)
-        predicted_x0_logits = self.model_predict(x_t, t, cond, S=None) # use masks, not S
+        predicted_x0_logits = self.model_predict(x_t, t, cond, S)
         true_q_posterior_logits = self.q_posterior_logits(x, x_t, t, S)
         pred_q_posterior_logits = self.q_posterior_logits(predicted_x0_logits, x_t, t, S)
 
         # get kls and loss
-        kl = kls(true_q_posterior_logits, pred_q_posterior_logits, self.eps)
-        weight = - self.beta_scale(t) * self.alpha_scale(t) / (1 - self.alpha_scale(t))
-        weight = (S.swapaxes(0, -1) * weight).swapaxes(0, -1) # mult by S to ignore S=0
+        kl = kls(true_q_posterior_logits, pred_q_posterior_logits, self.eps) # shape x
+        weight = self.beta(t) * self.alpha(t) / (1 - self.alpha(t)) #mult by p(S=1|t)
+        weight = (S.swapaxes(0, -1) * weight).swapaxes(0, -1)
         vb_loss = (kl * weight).mean() * self.t_max
 
         # Also calculate cross entropy loss
@@ -51,8 +57,8 @@ class MaskingDiffusion(SchdeuleCondition): #schedule conditioning is True!
 
     def sample_with_image_sequence(self, x, cond=None, trans_step=1, stride=10):
         t = self.t_max * torch.ones(x.shape[0], device=x.device)
-        S = sample_n_transitions_cont(self.alpha_scale, x[0].flatten().shape[0], t)
-        S = S.swapaxes(0, 1).reshape(*x.shape).long()
+        S = sample_n_transitions_cont(self.alpha, x[0].flatten().shape[0], t)
+        S = (S>0).swapaxes(0, 1).reshape(*x.shape).long() # this is the only line changed
         steps = 0
         images = []
         pbar = tqdm(total=S.sum(-1).sum(-1).sum(-1).max().item(), unit="iteration",
@@ -73,7 +79,7 @@ class MaskingDiffusion(SchdeuleCondition): #schedule conditioning is True!
             pbar.update(trans_step)
             steps += 1
             if steps % stride == 0:
-                images.append(x)
+                images.append(torch.clone(x))
         pbar.close()
         # if last step is not divisible by stride, we add the last image.
         if steps % stride != 0: