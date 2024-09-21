fix wrong backprop

d3pm_sc/.ipynb_checkpoints/trainer-checkpoint.py
@@ -71,9 +71,9 @@ class DiffusionTrainer(pl.LightningModule):
     def calc_p0(self, dataloader):
         # get stationary dist
         p0 = torch.ones(self.num_classes)
-        pbar = tqdm(total=2e5)
+        pbar = tqdm(total=2e6)
         for i, batch in tqdm(enumerate(dataloader)):
-            if p0.sum() > 2e5:  
+            if p0.sum() > 2e6:  
                 break
             if isinstance(batch, tuple): #image datasets
                 x, _ = batch

d3pm_sc/ct_sched_cond_sparse_k.py
@@ -80,6 +80,8 @@ class ScheduleConditionSparseK(ContinuousTimeDiffusion): #schedule conditioning 
         def K_T_operator(K_T, x_0_probs, stat): # assumes x_0_probs are normalized
             stat_broadcast = stat.view((self.num_classes,) + (1,) * (x_0_probs.dim()-1))
             return (1-self.up) * (K_T @ x_0_probs) + self.up * stat_broadcast
+        def K_operator_vec(K, x_t_probs, stat):            
+            return (1-self.up) * (K @ x_t_probs) + self.up * stat @ x_t_probs
         def K_operator(x_t_index):
             struct = self.K_coo.index_select(1, x_t_index.flatten()
                                             ).to_dense().T.reshape(
@@ -142,7 +144,7 @@ class ScheduleConditionSparseK(ContinuousTimeDiffusion): #schedule conditioning 
                               <= S.flatten().unsqueeze(0))
                 for i in range(max_power):
                     active = power_mask[i]
-                    x_grad[:, active] = self.K_T_operator(self.K, x_grad[:, active], self.stat)
+                    x_grad[:, active] = self.K_operator_vec(self.K, x_grad[:, active], self.stat)
                 return None, x_grad.T.reshape(shape).to(torch.bfloat16)
         return K_T_power_mult_class.apply(S, x_0)
     